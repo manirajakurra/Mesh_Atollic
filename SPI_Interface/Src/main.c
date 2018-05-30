a5f296be821799cab54894efca4dfe8d028fbb11
rajzz2512 <mani.raja2512@gmail.com>
2018-05-30 16:17:59 -0400

    first Commit

---
+++ SPI_Interface/Src/main.c
@@ -0,0 +1,413 @@
+/**
+  ******************************************************************************
+  * @file    SPI/SPI_FullDuplex_ComPolling/Src/main.c
+  * @author  MCD Application Team
+  * @brief   This sample code shows how to use STM32F3xx SPI HAL API to transmit
+  *          and receive a data buffer with a communication process based on
+  *          Polling transfer.
+  *          The communication is done using 2 Boards.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32F3xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup SPI_FullDuplex_ComPolling
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Uncomment this line to use the board as master, if not it is used as slave */
+//#define MASTER_BOARD
+
+/* Private variables ---------------------------------------------------------*/
+/* SPI handler declaration */
+SPI_HandleTypeDef SpiHandle;
+
+//SPI commands
+#define _NRF24L01P_SPI_CMD_RD_REG            0x00
+#define _NRF24L01P_SPI_CMD_WR_REG            0x20
+#define _NRF24L01P_SPI_CMD_RD_RX_PAYLOAD     0x61
+#define _NRF24L01P_SPI_CMD_WR_TX_PAYLOAD     0xa0
+#define _NRF24L01P_SPI_CMD_FLUSH_TX          0xe1
+#define _NRF24L01P_SPI_CMD_FLUSH_RX          0xe2
+#define _NRF24L01P_SPI_CMD_REUSE_TX_PL       0xe3
+#define _NRF24L01P_SPI_CMD_R_RX_PL_WID       0x60
+#define _NRF24L01P_SPI_CMD_W_ACK_PAYLOAD     0xa8
+#define _NRF24L01P_SPI_CMD_W_TX_PYLD_NO_ACK  0xb0
+#define _NRF24L01P_SPI_CMD_ACTIVATE 		 0x50
+
+//register
+#define _NRF24L01P_REG_CONFIG                0x00
+#define _NRF24L01P_REG_EN_AA                 0x01
+#define _NRF24L01P_REG_EN_RXADDR             0x02
+#define _NRF24L01P_REG_SETUP_AW              0x03
+#define _NRF24L01P_REG_SETUP_RETR            0x04
+#define _NRF24L01P_REG_RF_CH                 0x05
+#define _NRF24L01P_REG_RF_SETUP              0x06
+#define _NRF24L01P_REG_STATUS                0x07
+#define _NRF24L01P_REG_OBSERVE_TX            0x08
+#define _NRF24L01P_REG_CD                    0x09
+#define _NRF24L01P_REG_RX_ADDR_P0            0x0a
+#define _NRF24L01P_REG_RX_ADDR_P1            0x0b
+#define _NRF24L01P_REG_RX_ADDR_P2            0x0c
+#define _NRF24L01P_REG_RX_ADDR_P3            0x0d
+#define _NRF24L01P_REG_RX_ADDR_P4            0x0e
+#define _NRF24L01P_REG_RX_ADDR_P5            0x0f
+#define _NRF24L01P_REG_TX_ADDR               0x10
+#define _NRF24L01P_REG_RX_PW_P0              0x11
+#define _NRF24L01P_REG_RX_PW_P1              0x12
+#define _NRF24L01P_REG_RX_PW_P2              0x13
+#define _NRF24L01P_REG_RX_PW_P3              0x14
+#define _NRF24L01P_REG_RX_PW_P4              0x15
+#define _NRF24L01P_REG_RX_PW_P5              0x16
+#define _NRF24L01P_REG_FIFO_STATUS           0x17
+#define _NRF24L01P_REG_DYNPD                 0x1c
+#define _NRF24L01P_REG_FEATURE               0x1d
+
+#define SET_CSN HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET); // SET CSN
+#define RESET_CSN HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET); // RESET CSN
+#define SET_CE HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); // SET CE
+#define RESET_CE HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); // RESET CE
+
+
+
+
+/* Buffer used for transmission */
+uint8_t aTxBuffer[] = "****SPI - Two Boards communication based on Polling **** SPI Message ******** SPI Message ******** SPI Message ****";
+
+/* Buffer used for reception */
+uint8_t aRxBuffer[BUFFERSIZE];
+
+#define MASTER_BOARD 1
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void Error_Handler(void);
+
+void MX_GPIO_Init();
+void send_data_to_spi(uint8_t,uint8_t);
+uint8_t receive_data_to_spi(uint8_t, uint8_t);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+
+{
+
+	uint8_t spiCmd = 0;
+	uint8_t spiData = 0;
+  /* STM32F3xx HAL library initialization:
+       - Configure the Flash prefetch
+       - Configure the Systick to generate an interrupt each 1 msec
+       - Set NVIC Group Priority to 4
+       - Low Level Initialization
+     */
+  HAL_Init();
+  
+  /* Configure the system clock to 64 MHz */
+  SystemClock_Config();
+
+  /* Configure LED2 */
+  BSP_LED_Init(LED2);
+  MX_GPIO_Init();
+  /*##-1- Configure the SPI peripheral #######################################*/
+  /* Set the SPI parameters */
+  SpiHandle.Instance               = SPIx;
+  SpiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+  SpiHandle.Init.Direction         = SPI_DIRECTION_2LINES;
+  SpiHandle.Init.CLKPhase          = SPI_PHASE_1EDGE;
+  SpiHandle.Init.CLKPolarity       = SPI_POLARITY_LOW;
+  SpiHandle.Init.DataSize          = SPI_DATASIZE_8BIT;
+  SpiHandle.Init.FirstBit          = SPI_FIRSTBIT_MSB;
+  SpiHandle.Init.TIMode            = SPI_TIMODE_DISABLE;
+  SpiHandle.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
+  SpiHandle.Init.CRCPolynomial     = 7;
+  SpiHandle.Init.CRCLength         = SPI_CRC_LENGTH_8BIT;
+  SpiHandle.Init.NSS               = SPI_NSS_SOFT;
+  SpiHandle.Init.NSSPMode          = SPI_NSS_PULSE_DISABLE;
+
+#ifdef MASTER_BOARD
+  SpiHandle.Init.Mode = SPI_MODE_MASTER;
+#else
+  SpiHandle.Init.Mode = SPI_MODE_SLAVE;
+#endif /* MASTER_BOARD */
+
+  if(HAL_SPI_Init(&SpiHandle) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+
+#ifdef MASTER_BOARD
+  /* Configure User push-button button */
+  BSP_PB_Init(BUTTON_USER,BUTTON_MODE_GPIO);
+
+  /* Wait for User push-button press before starting the Communication */
+  while (BSP_PB_GetState(BUTTON_USER) != GPIO_PIN_RESET)
+  {
+    BSP_LED_Toggle(LED2);
+    HAL_Delay(100);
+  }
+  BSP_LED_Off(LED2);
+#endif /* MASTER_BOARD */
+
+  /*##-2- Start the Full Duplex Communication process ########################*/  
+  /* While the SPI in TransmitReceive process, user can transmit data through 
+     "aTxBuffer" buffer & receive data through "aRxBuffer" */
+  /* Timeout is set to 5S */
+
+  //configuration of nRF24L01
+  //CONFIG
+  //PRIM_RX = 0
+  //PWR_UP = 1
+  //MASK_MAX_RT = 1
+  SET_CE;
+  spiCmd = _NRF24L01P_SPI_CMD_WR_REG | _NRF24L01P_REG_CONFIG;
+  spiData = 26; //'00011010'
+  send_data_to_spi(spiCmd, spiData);
+
+  //to read the content of the CONFIG register in nrf24L01 module
+  spiCmd = _NRF24L01P_SPI_CMD_RD_REG | _NRF24L01P_REG_CONFIG;
+  spiData = 0; //'00000000'
+  spiData = receive_data_to_spi(spiCmd, spiData);
+
+  //EN_AA
+  // enable auto ACK
+  spiCmd = _NRF24L01P_SPI_CMD_WR_REG | _NRF24L01P_REG_EN_AA;
+ // spiData = 63; //'00111111'
+  send_data_to_spi(spiCmd, spiData);
+
+  //EN_RXADDR
+  //Enable data pipe 0 and 1
+  spiCmd = _NRF24L01P_SPI_CMD_WR_REG | _NRF24L01P_REG_EN_RXADDR;
+  spiData = 3; //'00000011'
+  send_data_to_spi(spiCmd, spiData);
+
+  //FLUSH_TX
+  spiCmd = _NRF24L01P_SPI_CMD_FLUSH_TX;
+  spiData = 0; //'00000011'
+  send_data_to_spi(spiCmd, spiData);
+
+  //FLUSH_RX
+  spiCmd = _NRF24L01P_SPI_CMD_FLUSH_RX;
+  spiData = 0; //'00000011'
+  send_data_to_spi(spiCmd, spiData);
+
+  /* Infinite loop */
+  while (1)
+  {
+	  //W_TX_PAYLOAD
+      spiCmd = _NRF24L01P_SPI_CMD_WR_TX_PAYLOAD;
+      spiData = 12; //'00001100'
+      send_data_to_spi(spiCmd, spiData);
+/*	  HAL_SPI_TransmitReceive(&SpiHandle, (uint8_t*)aTxBuffer, (uint8_t *)aRxBuffer, 1, 0);
+	  aTxBuffer[0] = aRxBuffer[0] | 0x70;
+
+	  if (aRxBuffer[0]) BSP_LED_On(LED2); else BSP_LED_Off(LED2);*/
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  while(1)
+  {
+    /* Toggle LED2 for error */
+    BSP_LED_Toggle(LED2);
+    HAL_Delay(1000);
+
+  }
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 64000000
+  *            HCLK(Hz)                       = 64000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSI Frequency(Hz)              = 8000000
+  *            PREDIV                         = RCC_PREDIV_DIV2 (2)
+  *            PLLMUL                         = RCC_PLL_MUL16 (16)
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  
+  /* HSI Oscillator already ON after system reset, activate PLL with HSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+}
+/**
+  * @brief  Compares two buffers.
+  * @param  pBuffer1, pBuffer2: buffers to be compared.
+  * @param  BufferLength: buffer's length
+  * @retval 0  : pBuffer1 identical to pBuffer2
+  *         >0 : pBuffer1 differs from pBuffer2
+  */
+
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PB3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA0 */
+  //CE
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA1 */
+  //CSN
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+ void send_data_to_spi(uint8_t spiCmd,uint8_t spiData)
+ {
+	RESET_CE;
+	SET_CSN;
+	HAL_Delay(10);
+	SET_CE;
+	RESET_CSN;
+
+	HAL_SPI_Transmit(&SpiHandle, &spiCmd, 1, 0);
+    HAL_SPI_Transmit(&SpiHandle, &spiData, 1, 0);
+ }
+
+
+ uint8_t receive_data_to_spi(uint8_t spiCmd, uint8_t spiData)
+ {
+	RESET_CE;
+	SET_CSN;
+	HAL_Delay(10);
+	SET_CE;
+	RESET_CSN;
+	uint8_t Rxd_Data = 0;
+
+	HAL_SPI_TransmitReceive(&SpiHandle, &spiCmd, &Rxd_Data, 1, 0);
+	HAL_SPI_TransmitReceive(&SpiHandle, &spiData, &Rxd_Data, 1, 0);
+	return Rxd_Data;
+ }
+
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
